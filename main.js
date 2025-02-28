5bf5aa6687c6512533d4f1834c33d1442cd0187d
seb <13894287+Sebamor@users.noreply.github.com>
2025-02-28 13:36:24 -0800

    Modal and responsiveness fixes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Burger menu collapse
 function burgerMenu() {
     let burger = document.getElementById('nav-links');
     if (burger.style.display === 'none') {
@@ -7,3 +8,48 @@ function burgerMenu() {
         burger.style.display = 'none';
     }
 }
+
+// Get the modal
+var modal1 = document.getElementById("modal1");
+var modal2 = document.getElementById("modal2");
+var modal3 = document.getElementById("modal3");
+var modal = document.querySelector(".modal");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("extra-info1");
+var btn2 = document.getElementById("extra-info2");
+var btn3 = document.getElementById("extra-info3");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1 || event.target == modal2 || event.target == modal3) {
+        modal1.style.display = "none";
+        modal2.style.display = "none";
+        modal3.style.display = "none";
+    }
+} 
